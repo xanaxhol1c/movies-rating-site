57f378cb561bdeca3a93d22bbc339eeecb548c2d
xanaxhol1c <loram.hoptan@gmail.com>
2025-06-29 21:38:23 +0300

    added test index page + admin panel fix

--- movies_rating_site/main/admin.py
+++ movies_rating_site/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Category, Movie
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug' : ('name',)}
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'rating', 'description', 'image', 'release_date']
+    list_filter = ['name', 'category', 'rating']
+    prepopulated_fields = {'slug' : ('name',)}
--- movies_rating_site/movies_rating_site/settings.py
+++ movies_rating_site/movies_rating_site/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
