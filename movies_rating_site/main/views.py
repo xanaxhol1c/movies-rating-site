e1294641c6f234cec4ecda9a46e7e8a06a0542cf
xanaxhol1c <loram.hoptan@gmail.com>
2025-06-30 18:12:18 +0300

    created top_chart view with template

--- movies_rating_site/main/views.py
+++ movies_rating_site/main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+def top_chart(request):
+    movies = Movie.objects.all()
+    return render(request, 'main/chart.html', {'movies' : movies})
--- movies_rating_site/movies_rating_site/settings.py
+++ movies_rating_site/movies_rating_site/settings.py
@@ -119,7 +119,9 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-MEDIA_ROOT = 'media/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
