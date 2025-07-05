8dc4f33d3bf69ebf55ba9cab9f66f36ae650f5c5
xanaxhol1c <loram.hoptan@gmail.com>
2025-07-05 18:34:55 +0300

    created docker files for project

--- movies_rating_site/movies_rating_site/settings.py
+++ movies_rating_site/movies_rating_site/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -129,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
